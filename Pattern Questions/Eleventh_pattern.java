d415295b0879fa4166385bf2812653bb464c3627
Dakshay2000 <dax.batra88@gmail.com>
2022-01-31 23:51:43 +0530

    Tenth

---
+++ Pattern Questions/Eleventh_pattern.java
@@ -0,0 +1,52 @@
+public class Eleventh_pattern {
+    public static void main(String[] args) {
+        int n = 5;
+        int nst1 = 1;
+        int nst2 = 0;
+        int nsp1 = n/2;
+        int nsp2 = n/2;
+
+        for(int i = 1; i<=n; i++)
+        {
+            //For spaces
+            for(int j = 1; j <= nsp1; j++)
+            {
+                System.out.print("\t");
+            }
+            
+            //For stars
+            for(int j = 1; j <= nst1; j++)
+            {
+                System.out.print("*\t");
+            }
+
+            //For spaces
+            for(int j = 1; j <= nsp2; j++)
+            {
+                System.out.print("\t");
+            }
+
+            //For stars
+            for(int j = 1; j <= nst2; j++)
+            {
+                System.out.print("*\t");
+            }
+
+            if(nst2 != 1){
+                nst2 = 1;
+            }
+
+            if(i <= nsp1)
+            {
+                nsp1 = nsp1 - 1;
+                nsp2 = nsp2 + 2;
+            }
+
+            else{
+                nsp1 = nsp1 + 1;
+                nsp2 = nsp2 - 2;
+            }
+            System.out.println();
+        }
+    }
+}
