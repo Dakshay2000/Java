b6ea8c442956c76dea6c9153c0f0ddf6f2e774ab
Dakshay2000 <dax.batra88@gmail.com>
2022-02-04 21:15:55 +0530

    Tenth

---
+++ Pattern Questions/Fifteen_pattern.java
@@ -0,0 +1,12 @@
+public class Fifteen_pattern {
+    public static void main(String[] args) {
+        int n = 5;
+        //int val = 1;
+
+        for(int i = 1; i <= 10; i++)
+        {
+            int result = n * i;
+            System.out.println(n + " * " + i + " = " + result);
+        }
+    }
+}
