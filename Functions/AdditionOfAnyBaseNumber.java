068af6e3817b98a9453abe279d91038098203999
Dakshay2000 <dax.batra88@gmail.com>
2022-02-14 20:19:59 +0530

    Tenth

---
+++ Functions/AdditionOfAnyBaseNumber.java
@@ -0,0 +1,40 @@
+public class AdditionOfAnyBaseNumber {
+    public static int addition(int n1, int n2, int base) {
+        
+        int carry = 0;
+        int ans = 0;
+        int power = 1;
+        
+
+        while(n1 != 0 || n2 != 0 || carry != 0)
+        {
+            int l1 = n1%10;
+            int l2 = n2%10;
+
+            n1 = n1/10;
+            n2 = n2/10;
+
+            int add = l1 + l2 + carry;
+
+            int mod = add % base;
+            int div = add / base;
+            carry = div;
+            
+            ans = ans + (mod * power);
+            power = power * 10;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n1 = 724;
+        int n2 = 72;
+        int base = 8;
+
+        int result = addition(n1, n2, base);
+
+        System.out.println(result);
+
+    
+    }
+}
---
+++ Functions/Practice.java
@@ -0,0 +1,10 @@
+public class Practice {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+
+    int ans = b/a;
+    System.out.println(ans);
+    }
+}
